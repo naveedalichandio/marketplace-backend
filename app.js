1c4ac4d0ee1411f153bb4dbd38d0c2f151503e8d
Naveed Ali <naveedalichandio20@gmail.com>
2025-01-20 21:16:39 +0500

    imported pg for configurations

--- app.js
+++ app.js
@@ -12,40 +12,41 @@ db.sequelize
 
   .then(() => {
     console.log("Database synchronized!");
-
-    app.use(express.static(path.join(__dirname, "/public")));
-    app.use(function (err, req, res, next) {
-      if (err.name === "UnauthorizedError") {
-        res.status(401);
-        res.json({ message: err.name + ": " + err.message });
-      }
-    });
-    app.use(bodyParser.json()); // to support JSON-encoded bodies
-    app.use(cors());
-    app.use(
-      bodyParser.urlencoded({
-        extended: true,
-      })
-    );
-
-    var route = require("./scr/routes");
-    app.use(route);
-
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "../../public", "index.html"));
-    });
-
-    // no stacktraces leaked to user
-    app.use(function (err, req, res, next) {
-      res.status(err.status || 500);
-      res.send({
-        error: { code: err.code, msg: err.message },
-      });
-    });
   })
   .catch((error) => {
     console.error("Error syncing database:", error.message);
   });
+
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(function (err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    res.status(401);
+    res.json({ message: err.name + ": " + err.message });
+  }
+});
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+var route = require("./scr/routes");
+app.use(route);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../../public", "index.html"));
+});
+
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send({
+    error: { code: err.code, msg: err.message },
+  });
+});
+
 const PORT = 8080;
 const HOST = "0.0.0.0";
 
